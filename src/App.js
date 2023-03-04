ebeb89de3a83095c82177b722a1191b3560971fe
DhamaleSiddhesh <dhamalesiddhesh07@gmail.com>
2023-03-04 20:57:00 +0530

    FacebookLoginPage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div class="display">
+            <div class="caption">
+                <div class="facebook">facebook</div>
+                <div class="text">Facebook helps you connect and
+                    <div>share with people in your life</div>
+                </div>
+            </div>
+            <div class="box ">
+                <input class="button" type="email" placeholder="Email adress or phone number" />
+                <input class="button" type="password" placeholder="Password" />
+                <button class="login">Log In</button>
+                <div class="forgetP">Forgotten password</div>
+                <hr />
+                <div class="Align"><button class="bodybutton">create New Account</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
